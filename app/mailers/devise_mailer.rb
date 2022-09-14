6b36c42a950df752f727d2fa6263ec99fac40659
ldesierra <83076120+ldesierra@users.noreply.github.com>
2022-09-14 20:27:26 -0300

    Fix class name error on reset password (#22)

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,7 +2,7 @@ class DeviseMailer < Devise::Mailer
   default from: 'default@gmail.com'
 
   def reset_password_instructions(record, token, opts = {})
-    @user_kind = record.class_name
+    @user_kind = record.class.name
     super
   end
 end
