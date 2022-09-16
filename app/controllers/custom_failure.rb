4a4ea087200bdb088b43ce22f014004be55c22d7
ldesierra <83076120+ldesierra@users.noreply.github.com>
2022-09-15 21:16:40 -0300

    Fix admin not redirecting when not authenticated (#27)

--- app/controllers/custom_failure.rb
+++ app/controllers/custom_failure.rb
@@ -1,5 +1,7 @@
 class CustomFailure < Devise::FailureApp
   def respond
+    return redirect_to new_admin_user_session_path if params[:controller].start_with?('admin')
+
     self.status = 401
     self.content_type = 'json'
     self.response_body = '{"error" : "authentication error"}'
