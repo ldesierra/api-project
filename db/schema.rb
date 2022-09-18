d859241bdec3bc937353d82b12742c44d22c69e4
trodriguez10 <95036319+trodriguez10@users.noreply.github.com>
2022-09-18 13:09:31 -0300

    FIxing restaurant migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_16_174540) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_155812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,14 +111,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_16_174540) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.integer "status", default: 0
     t.integer "phone_number"
     t.string "location"
     t.string "logo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_restaurants_on_deleted_at"
+    t.index ["name"], name: "index_restaurants_on_name", unique: true
   end
 
   add_foreign_key "open_hours", "restaurants"
